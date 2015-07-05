60844999ffdc7cfa66e70b9e1a62a9c0572cf2c2
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-07-05 15:24:44 +0300

    specs

--- spec/yakovenko_riverbattle_spec.rb
+++ spec/yakovenko_riverbattle_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe YakovenkoRiverbattle do
+
+  subject(:game) 	{ Game.new }
+
   it 'has a version number' do
     expect(YakovenkoRiverbattle::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a start method in the Game class' do
+    expect(Game.method_defined? :start).to be_truthy
   end
+
+  it 'includes the Symbols module in the Game class' do
+    expect(Game.included_modules).to include(Symbols)
+  end
+
 end
