b65cc43dbb16396c11a14b2cefde3d32c354b2dc
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-07-05 16:04:57 +0300

    make executable

--- spec/yakovenko_riverbattle_spec.rb
+++ spec/yakovenko_riverbattle_spec.rb
@@ -2,18 +2,45 @@ require 'spec_helper'
 
 describe YakovenkoRiverbattle do
 
-  subject(:game) 	{ Game.new }
-
   it 'has a version number' do
     expect(YakovenkoRiverbattle::VERSION).not_to be nil
   end
 
-  it 'has a start method in the Game class' do
-    expect(Game.method_defined? :start).to be_truthy
-  end
+    context 'Game class' do
 
-  it 'includes the Symbols module in the Game class' do
-    expect(Game.included_modules).to include(Symbols)
-  end
+        it 'has a start method in the Game class' do
+            expect(Game.method_defined? :start).to be_truthy
+        end
+
+        it 'includes the Symbols module in the Game class' do
+            expect(Game.included_modules).to include(Symbols)
+        end
+
+    end
+
+
+    context 'Move class' do
+
+        it 'contains :hit attribute' do
+            expect(Move.method_defined? :hit).to be_truthy
+        end
+
+        it 'contains :index attribute' do
+            expect(Move.method_defined? :index).to be_truthy
+        end
+
+        it 'accepts 2 arguments when initialized' do
+            expect(Move.instance_method(:initialize).arity).to eq(2)
+        end
+ 
+    end
+
+    context 'Human class' do
+
+        it 'contains 9 methods' do
+            expect(Player.instance_methods.size).to eq(9)
+        end
+
+    end
 
 end
